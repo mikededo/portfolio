b09705ae7112c2901fba92da8b586f95e67ea98f
Miquel de Domingo <miquelddg@gmail.com>
2025-01-16 22:21:59 +0100

    feat: initial home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,24 @@
     const { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex shrink-0 flex-col h-screen">
+    <header class="w-full h-header bg-crust/75 flex justify-between px-4 py-3 z-10">
+        <p class="font-semibold text-xl">
+            @mikededo
+        </p>
+        <nav class="flex gap-6 items-center">
+            <a href="/" class="text-sm text-mauve">home</a>
+            <a href="/projects" class="text-sm hover:text-mauve">projects</a>
+            <a href="/about" class="text-sm hover:text-mauve">about</a>
+        </nav>
+    </header>
+    <main class="bg-base h-full flex-1">
+        <div class="container h-full w-full">
+            {@render children()}
+        </div>
+    </main>
+    <footer class="bg-crust/75 z-10 flex p-3 justify-end">
+        <p class="text-sm text-mauve leading-4">@mikededo</p>
+    </footer>
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-full justify-center flex-col gap-2">
+    <div class="flex items-center gap-4">
+        <span class="text-overlay1">1</span>
+        <p>Hi, I&apos;m</p>
+    </div>
+    <div class="flex items-end gap-4">
+        <span class="text-overlay1">2</span>
+        <h1 class="text-7xl font-bold">Miquel de Domingo</h1>
+    </div>
+    <div class="flex items-center gap-4">
+        <span class="text-overlay1">3</span>
+        <p>a software engineer based in Girona</p>
+    </div>
+</div>
+
+<svg
+    class="absolute inset-0 h-screen w-screen"
+    viewBox="0 0 1440 1024"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <g opacity="0.35" filter="url(#filter0_f_3_3774)" class="fill-blue">
+        <rect
+            x="899.434"
+            y="52"
+            width="401.074"
+            height="416.279"
+            transform="rotate(8.20843 899.434 52)"
+        />
+        <path d="M109.517 443.151L231.791 748.988L-94.2085 701.961L109.517 443.151Z" class="fill-red" />
+        <circle
+            cx="1318.06"
+            cy="571.061"
+            r="308.213"
+            transform="rotate(8.20843 1318.06 571.061)"
+            class="fill-mauve"
+        />
+        <path d="M520 875L691.473 974V1172L520 1271L348.527 1172V974L520 875Z" class="fill-overlay0" />
+    </g>
+    <defs>
+        <filter id="filter0_f_3_3774"
+            x="-294.208"
+            y="-200"
+            width="2120.52"
+            height="1671"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+        >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal"
+                in="SourceGraphic"
+                in2="BackgroundImageFix"
+                result="shape"
+            />
+            <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_3_3774" />
+        </filter>
+    </defs>
+</svg>
+
