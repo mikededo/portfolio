4b8aa57b9585156f65f348a68c0982f72f058215
Miquel de Domingo <miquelddg@gmail.com>
2025-08-13 19:04:15 +0200

    fix: correct tracking analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,6 @@ import { dev } from '$app/environment';
 import { DENY_ANALYTICS } from '$lib/components';
 
 injectAnalytics({
-  beforeSend: (e) => window.localStorage.getItem(DENY_ANALYTICS) ? null : e,
+  beforeSend: (e) => window.localStorage.getItem(DENY_ANALYTICS) === 'true' ? null : e,
   mode: dev ? 'development' : 'production'
 });
