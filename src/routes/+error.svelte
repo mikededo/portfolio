40613aa534009905effb22bf3a516ead41274231
Miquel de Domingo <miquelddg@gmail.com>
2025-08-20 09:47:40 +0200

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<main class="flex h-[calc(100dvh-4rem)] flex-col justify-center">
+    <h1 class="text-lg font-semibold">Oops, something went wrong</h1>
+    <p class="text-sm">Get back to the <a href="/">homepage</a>.</p>
+</main>
