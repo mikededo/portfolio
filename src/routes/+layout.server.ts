7e4db32ae242894a1032785a1b0149463f8407df
Miquel de Domingo <miquelddg@gmail.com>
2025-11-23 11:26:18 +0100

    fix: remove `prerender` from layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,8 +3,6 @@ import type { LayoutServerLoad } from './$types';
 import { getIsRecoveryPeriod, getWeeklyActivitySummary } from '$lib/server';
 import { getThemeCookie } from '$lib/utils/theme';
 
-export const prerender = true;
-
 export const load: LayoutServerLoad = async ({ cookies }) => {
   const [stats, isRecoveryPeriod] = await Promise.all([
     getWeeklyActivitySummary(),
