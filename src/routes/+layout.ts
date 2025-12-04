a31e48bc13e2e505938ad456a51d86e31bb2655e
Miquel de Domingo <miquelddg@gmail.com>
2025-12-04 17:26:23 +0100

    chore: add speed insights + prerender

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,10 @@
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 import { dev } from '$app/environment';
 import { DENY_ANALYTICS } from '$lib/components';
 
+injectSpeedInsights();
 injectAnalytics({
   beforeSend: (e) => window.localStorage.getItem(DENY_ANALYTICS) === 'true' ? null : e,
   mode: dev ? 'development' : 'production'
