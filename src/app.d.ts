b57f0a9d0916922d147090a3fc0003edc8a2b332
Miquel de Domingo <miquelddg@gmail.com>
2025-12-08 09:25:37 +0100

    feat: add typesafe routes

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,9 @@
 import type { ClassValue } from 'svelte/elements'
 
+import type { Pathname } from '$app/types'
+
+type NonApiRoutes = Exclude<Pathname, `/api${string}`>
+
 declare global {
   namespace App {
     // interface Error {}
@@ -7,6 +11,15 @@ declare global {
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
+  }
+
+  namespace svelteHTML {
+    // This adds typed urls for anchor elements
+    interface IntrinsicElements {
+      a: {
+        href?: ({} & string) | NonApiRoutes | null
+      } & Omit<HTMLAnchorElements, 'href'>
+    }
   }
 }
 
